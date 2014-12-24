5f590909a35f038161dc59d60ceec7c8ce458b0b
Fabian Williams <fabian@adotob.com>
2014-12-24 00:40:34 -0500

    Extended Model and Class and Works
    Extended model and class by adding two fields, launched app and DB in
    Azure created with the new fields, ran app and app has them. was able to
    add and delete items using client app on iphone simulator

---
+++ alphatodoService/App_Start/WebApiConfig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Mobile.Service;
+using alphatodoService.DataObjects;
+using alphatodoService.Models;
+
+namespace alphatodoService
+{
+    public static class WebApiConfig
+    {
+        public static void Register()
+        {
+            // Use this class to set configuration options for your mobile service
+            ConfigOptions options = new ConfigOptions();
+
+            // Use this class to set WebAPI configuration options
+            HttpConfiguration config = ServiceConfig.Initialize(new ConfigBuilder(options));
+
+            // To display errors in the browser during development, uncomment the following
+            // line. Comment it out again when you deploy your service for production use.
+            // config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            
+            Database.SetInitializer(new alphatodoInitializer());
+        }
+    }
+
+    public class alphatodoInitializer : ClearDatabaseSchemaIfModelChanges<alphatodoContext>
+    {
+        protected override void Seed(alphatodoContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Extended Item One", Complete = false, Owner = "Fabian Williams", Description = "Alpha"},
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Extended Item Two", Complete = false, Owner = "Gabby Williams", Description = "Bravo"},
+
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
+
---
+++ alphatodoService/DataObjects/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace alphatodoService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
